2f42c7c65cd9127dba3d471141f727968d23e523
victorh-morales@hotmail.com <victorh-morales@hotmail.com>
2020-10-22 23:47:10 -0600

    Allow estatus -1 and conv. to integer

--- src/models/viaje.js
+++ src/models/viaje.js
@@ -1,11 +1,8 @@
 const viaje = (sequelize, DataTypes) => {
     const Viaje = sequelize.define('viaje', {
       id_estatus: {
-          type: DataTypes.INTEGER,
-          references: {
-          model: 'estatus', // 'estatus' refers to table name
-          key: 'id', // 'id' refers to column name in estatus table
-          },
+        type: DataTypes.STRING,
+        unique: false,
       },
       id_solicitante: {
           type: DataTypes.INTEGER,
